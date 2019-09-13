c18c39c10ee5f776d20824b22bc80aac48578873
Shruti <shrutiwadhekar@gmail.com>
2019-09-13 20:00:34 +0530

    Initial Files Uploaded

---
+++ User_SignUP.php
@@ -0,0 +1,13 @@
+<?php
+$name=$_POST['name'];
+$Add=$_POST['address'];
+$Email=$_POST['email'];
+$MobileNum=$_POST['mobile'];
+$BloodGroup=$_POST['group'];
+$BirthDate=$_POST['BirthDate'];
+$Height=$_POST['height'];
+$Weight=$_POST['weight'];
+$Gender=$_POST['gender'];
+echo $name;
+
+?>
